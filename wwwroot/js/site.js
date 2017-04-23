f6262f6561c2a47fc208df62ba3b16eb6c3b00aa
Oleg Grafov <olegraf@gmail.com>
2017-04-23 19:33:30 +0300

    removed not needed moment locales

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,22 +1,27 @@
 ﻿//// Write your Javascript code.
-//$(function () {
-//    debugger;
-//    $('.UTCTime').each(function () {
-//        try {
-//            var text = $(this).html() + ' UTC'; //Append ' UTC'
+$(function () {
+    debugger;
+    $('.vp-datetime').each(function () {
+        $(this).datetimepicker({
+            //defaultDate: start.toDateString(),
+            format: 'MM.D.YYYY',
+            locale: 'ru'
+        });
+        //try {
+        //    var text = $(this).html() + ' UTC'; //Append ' UTC'
 
-//            var n = new Date(text);
+        //    var n = new Date(text);
 
-//            var dateStr = n.getDate() + "/" + n.getMonth() + "/" + n.getFullYear();
-//            $(this).html(dateStr + " " + n.toLocaleTimeString());
+        //    var dateStr = n.getDate() + "/" + n.getMonth() + "/" + n.getFullYear();
+        //    $(this).html(dateStr + " " + n.toLocaleTimeString());
 
-//            $(this).attr("title", "Converted from UTC " + text);
-//        }
-//        catch (ex) {
-//            console.warn("Error converting time", ex);
-//        }
-//    });
-//});
+        //    $(this).attr("title", "Converted from UTC " + text);
+        //}
+        //catch (ex) {
+        //    console.warn("Error converting time", ex);
+        //}
+    });
+});
 
 
 function goBack() {
