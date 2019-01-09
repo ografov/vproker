090f5a6b95507a923f0f12e58b914234ae408ab0
Oleg Grafov <olegraf@gmail.com>
2019-01-09 19:40:15 +0300

    maintains table show data per 50 on page

--- wwwroot/ts/utils.ts
+++ wwwroot/ts/utils.ts
@@ -1,21 +1,36 @@
 ﻿namespace vproker {
-export class Utils {
+    export class Utils {
         static dateTimeFormatter(value) {
+            if (!value) {
+                return "-";
+            }
             var date = new Date(value + 'Z');
             return '<span title="' + date + '">' + date.toLocaleString('ru-RU') + '</span>';
         }
 
         static dateFormatter(value) {
+            if (!value) {
+                return "-";
+            }
             var date = new Date(value + 'Z');
             return '<span title="' + date + '">' + date.toLocaleDateString('ru-RU') + '</span>';
         }
-        static getClientActions(id) {
 
-            var editBnt = '<a class="edit" href="/Client/Edit/' + id + '" title="Редактировать" style="margin: 3px"><i class="glyphicon glyphicon-edit"></i></a>';
-            var detailsBtn = '<a class="info-sign" href="/Client/Details/' + id + '" title="Просмотр" style="margin: 3px"><i class="glyphicon glyphicon-info-sign"></i></a>';
-            var deleteBtn = '<a class="edit" href="/Client/Delete/' + id + '" title="Удалить" style="margin: 3px"><i class="glyphicon glyphicon-remove"></i></a>';
+        static getClientActions(id) {
+            return Utils.getActions("Client", id);
+        }
+
+        static getMaintainActions(id) {
+            return Utils.getActions("Maintain", id);
+        }
+
+        static getActions(controller, id) {
+
+            var editBnt = `<a class="edit" href="/${controller}/Edit/${id}" title="Редактировать" style="margin: 3px"><i class="glyphicon glyphicon-edit"></i></a>`;
+            var detailsBtn = `<a class="info-sign" href="/${controller}/Details/${id}" title="Просмотр" style="margin: 3px"><i class="glyphicon glyphicon-info-sign"></i></a>`;
+            var deleteBtn = `<a class="edit" href="/${controller}/Delete/${id}" title="Удалить" style="margin: 3px"><i class="glyphicon glyphicon-remove"></i></a>`;
 
             return editBnt + detailsBtn + deleteBtn;
         }
-}
+    }
 }
