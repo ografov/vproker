f479f65eaf3a57f877d6d8959f67fd8978118f46
Oleg Grafov <olegraf@gmail.com>
2019-11-14 21:43:20 +0300

    PassportCheck test

---
+++ Tests/PassportCheckTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using vproker.Services;
+
+namespace vproker_tests
+{
+    [TestFixture]
+    class PassportCheckTests
+    {
+        [Test]
+        public void InvalidKurskPassport()
+        {
+            Assert.IsFalse(PassportCheck.Validate("3804246970"), "The passport has to be invalid");
+        }
+    }
+}
