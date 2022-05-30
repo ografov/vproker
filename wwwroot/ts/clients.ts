f8d7fa8ab25b61647e5c5a3a6e7f9292365dffdb
aabramovv <antonabramow22@gmail.com>
2022-05-30 20:28:24 +0300

    added download clients button

---
+++ wwwroot/ts/clients.ts
@@ -0,0 +1,13 @@
+﻿/// <reference path="../../node_modules/@types/jquery/index.d.ts" />
+
+namespace vproker {
+    export class Client {
+        static render() {
+            const $downloadClientsReport = $('#downloadClients');
+            $downloadClientsReport.click(() => {
+                let downloadUrl = "./DownloadClients";
+                window.open(downloadUrl);
+            });
+        }
+    }
+}
